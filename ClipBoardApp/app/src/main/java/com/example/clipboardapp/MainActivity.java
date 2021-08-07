05d2e4ada0393b62d8cad42abf91b7f729844714
Ananthakrishnan <ananthakrishnan20121999@gmail.com>
2021-08-07 17:09:13 +0530

    push folder

---
+++ ClipBoardApp/app/src/main/java/com/example/clipboardapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.clipboardapp;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button b1,b2;
+    EditText t1,t2;
+    ClipboardManager myClipboard;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1=findViewById(R.id.TxtCopy);
+        t2=findViewById(R.id.TxtPaste);
+        b1=findViewById(R.id.BtnCopy);
+        b1.setOnClickListener(this);
+        b2=findViewById(R.id.BtnPaste);
+        b2.setOnClickListener(this);
+        myClipboard= (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+    }
+    public void onClick(View v)
+    {
+        if(v.equals(b1))
+        {
+            ClipData myClip;
+            String data = t1.getText().toString();
+            myClip = ClipData.newPlainText("text", data);
+            myClipboard.setPrimaryClip(myClip);
+            Toast.makeText(getBaseContext(),"Text Copied",Toast.LENGTH_LONG).show();
+        }
+
+        else if(v.equals(b2))
+        {
+            ClipData abc = myClipboard.getPrimaryClip();
+            ClipData.Item item = abc.getItemAt(0);
+            t2.setText(item.getText().toString());
+            Toast.makeText(getBaseContext(),"Text Pasted!",Toast.LENGTH_LONG).show();
+        }
+    }
+}
